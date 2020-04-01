73bf3816b0ca9c3906646cdc483b941d0d3fd0e1
ksdumont <ksdumont@gmail.com>
2020-04-01 10:54:08 -0700

    added tests w jest and enzyme

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,12 +4,17 @@ import Navbar from "./Navbar";
 const HomePage = props => {
   return (
     <main>
-      <Navbar {...props} />
+      <Navbar className="navigation" {...props} />
       <div className="container">
         <h2>RecommendIt HomePage</h2>
-        <p>Lorem Ipsum</p>
+        <p className="homepage">
+          Welcome to RecommendIt, a site for users to recommend Shows, Movies,
+          Books, Podcasts, Youtube Channels, SubReddits and Websites to each
+          other
+        </p>
       </div>
     </main>
   );
 };
+
 export default HomePage;
